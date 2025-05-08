ffcc1fcf7d3251fe84dd20854c127e2786313db6
Phil <phil89122@gmail.com>
2025-05-08 23:18:46 +0100

    Added all the games and a dropdown menu, squashed bugs

--- src/pages/QuickLinks.tsx
+++ src/pages/QuickLinks.tsx
@@ -2,7 +2,35 @@ import { useEffect, useState } from "react";
 
 function QuickLinks() {
   const [showGame, setShowGame] = useState(false);
+  const [showPapaGames, setshowPapaGames] = useState(false);
   const [currentGame, setCurrentGame] = useState("https://cdn.phi.me.uk/papasfreezeria[1].swf");
+  // list to store game names and server locations to auto create game select elements because i am lazy
+  let Papagamenames = [
+    "Papa's Freezeria",
+    "Papa's Burgeria",
+    "Papa's Cupcakeria",
+    "Papa's Hot Doggeria",
+    "Papa's Pancakeria",
+    "Papa's Pastaria",
+    "Papa's Pizzeria",
+    "Papa's Scooperia",
+    "Papa's Taco Mia",
+    "Papa's Wingeria",
+  ];
+
+  let Papagamenamelocations = [
+    "https://cdn.phi.me.uk/papasfreezeria[1].swf",
+    "https://cdn.phi.me.uk/PapasBurgeria.swf",
+    "https://cdn.phi.me.uk/PapasCupcakeria.swf",
+    "https://cdn.phi.me.uk/PapasHotDoggeria.swf",
+    "https://cdn.phi.me.uk/PapasPancakeria.swf",
+    "https://cdn.phi.me.uk/PapasPastaria.swf",
+    "https://cdn.phi.me.uk/PapasPizzeria.swf",
+    "https://cdn.phi.me.uk/PapasScooperia.swf",
+    "https://cdn.phi.me.uk/PapasTacoMia.swf",
+    "https://cdn.phi.me.uk/PapasWingeria.swf",
+  ];
+
 
   useEffect(() => {
     const handleKey = (e: KeyboardEvent) => {
@@ -10,7 +38,7 @@ function QuickLinks() {
         setShowGame((prev) => !prev);
       }
       if (e.key === "F1") {
-        window.location.href = "https://www.sparxmaths.uk";
+        window.location.href = "https://www.thedeanery.schoolsynergy.co.uk";
       }
       
     };
@@ -30,7 +58,7 @@ function QuickLinks() {
 
   if (showGame) {
     return (
-      <main id="main-site">
+      <main id="main-site" className="GamePage">
         <h1 className="CenterTitle">Quick Links</h1>
         <div style={{ width: "100%", maxWidth: "800px", margin: "0 auto" }} key={currentGame}>
           <object
@@ -48,10 +76,29 @@ function QuickLinks() {
           console.log("clicked");
           setCurrentGame("https://cdn.phi.me.uk/HappyWheels.swf");
           }} >Happy wheels</span>
-        <span className = "FancyLink" onClick={() => {
-          console.log("clicked");
-          setCurrentGame("https://cdn.phi.me.uk/papasfreezeria[1].swf");
-          }} >Papa's Freezeria</span>
+        <p 
+          className="ClickableHeadingLeft" 
+          onClick={() => setshowPapaGames(!showPapaGames)}
+        >
+          Papa's Cooking games 
+          <span className="DropdownArrow">{showPapaGames ? "▲" : "▼"}</span>
+        </p>
+        {showPapaGames && (
+          Papagamenames.map((game, index) => (
+            <span 
+              key={index} 
+              className="FancyLink" 
+              onClick={() => {
+                console.log("clicked");
+                setCurrentGame(Papagamenamelocations[index]);
+              }}
+            >
+              {game}
+            </span>
+          ))
+        )}
+        
+        
         <p className="BodyTextLeft">Press <code>g</code> again to go back to your links.</p>
       </main>
     );
